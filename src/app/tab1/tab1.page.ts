214d3888cf0257a5a8329fb684e0dd506621e662
Luis Felce <luisr.felce@gmail.com>
2022-10-19 09:44:30 +0200

    Fin de la sección

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
   constructor() {}
+
+  ngOnInit(): void {
+
+    // debugger es una instruccion que detiene el cargado de la pagina en ese punto.
+    // debugger;
+
+    let num = 10;
+
+    num = 20;
+    num = num + 2;
+
+     console.log(num);
+  }
 
 }
